eaaa3b82da583a20668f5a69b3d91ab8215eb878
khyarul <50608159+khyarul@users.noreply.github.com>
2020-03-28 19:10:39 +0700

    Add files via upload

---
+++ sentinel_main_3/sentinel_button.ino
@@ -0,0 +1,8 @@
+void initButton() {
+  pinMode(PB5, INPUT_PULLUP);
+  pinMode(PB4, INPUT_PULLUP);
+  pinMode(PB3, INPUT_PULLUP);
+  pinMode(PB11, INPUT_PULLUP);
+  pinMode(PB13, INPUT_PULLUP);
+  pinMode(PB12, INPUT_PULLUP);
+}
